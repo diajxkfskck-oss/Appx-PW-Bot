88d7c3297ba14e1faffa3a7cd4ef773e755d7613
diajxkfskck-oss <diajxkfskck@gmail.com>
2026-08-10 23:28:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = int(os.environ.get("API_ID", 20821267))
-api_hash = os.environ.get("API_HASH", "8723cdf433be176300044547ee6bab7a")
-bot_token = os.environ.get("BOT_TOKEN", "7757892840:AAHtk7qVt9r55Ovm-eYW71f1_ohNMdvUohc")
-auth_users = [int(x.strip()) for x in os.environ.get("AUTH_USERS", "7894095051").split(",") if x.strip().isdigit()]
+api_id = int(os.environ.get("API_ID", 31280586))
+api_hash = os.environ.get("API_HASH", "5f30188e8ff92b134ea343bb01eeaa90")
+bot_token = os.environ.get("BOT_TOKEN", "8852786935:AAGwiBi2KF4LTOmq9GvyfYes0qW58jJBuYA")
+auth_users = [int(x.strip()) for x in os.environ.get("AUTH_USERS", "8847993242").split(",") if x.strip().isdigit()]
 
 if not api_id: raise ValueError("Set API_ID env var!")
 if not api_hash: raise ValueError("Set API_HASH env var!")
